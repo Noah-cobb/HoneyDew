78274489bd2c02b947c529620870c21be9319962
AeroSoap <joshua.adler@digipen.edu>
2020-01-30 13:50:40 -0800

    Yesn't

---
+++ Dinometry Dash/Assets/Scripts/DinoJump.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DinoJump : MonoBehaviour
+{
+
+    public GameObject Ground;
+    Collider2D groundCol;
+    Collider2D dinoCol;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        groundCol = Ground.GetComponent<Collider2D>();
+        dinoCol = this.GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
