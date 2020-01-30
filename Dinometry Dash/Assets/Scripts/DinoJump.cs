4f08b7e8aba286b903d7ee8ed42eeced151d714b
AeroSoap <joshua.adler@digipen.edu>
2020-01-30 14:29:37 -0800

    Added dino boi

--- Dinometry Dash/Assets/Scripts/DinoJump.cs
+++ Dinometry Dash/Assets/Scripts/DinoJump.cs
@@ -4,21 +4,44 @@ using UnityEngine;
 
 public class DinoJump : MonoBehaviour
 {
+    public float JumpPower;
+    public float DownPower;
+    public int JumpFrames;
+
+    int jfs;
 
     public GameObject Ground;
     Collider2D groundCol;
     Collider2D dinoCol;
+    Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
         groundCol = Ground.GetComponent<Collider2D>();
-        dinoCol = this.GetComponent<Collider2D>();
+        dinoCol = GetComponent<Collider2D>();
+        rb = GetComponent<Rigidbody2D>();
+        jfs = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(dinoCol.IsTouching(groundCol))
+        {
+            jfs = JumpFrames;
+        }
+        if(jfs > 0 && Input.GetKey("space"))
+        {
+            jfs--;
+            rb.velocity = new Vector2(0, JumpPower);
+        } else
+        {
+            jfs = 0;
+        }
+        if(Input.GetKey("down"))
+        {
+            rb.velocity = new Vector2(0, -DownPower);
+        }
     }
 }
