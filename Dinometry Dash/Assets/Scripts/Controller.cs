28f4f4ddaf3e23bf4df3e2f722beca54a4a091ac
AeroSoap <joshua.adler@digipen.edu>
2020-01-31 14:14:21 -0800

    KILL ME!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    (With kindness?)

---
+++ Dinometry Dash/Assets/Scripts/Controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+    public float SpeedIncrease;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ObstacleMovement.Speed += SpeedIncrease * Time.deltaTime;
+    }
+}
---
+++ Dinometry Dash/Assets/Scripts/ObstacleMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMovement : MonoBehaviour
+{
+
+    public static float Speed = 5;
+
+    Transform trans;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trans = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        trans.position += new Vector3(-Speed * Time.deltaTime, 0);
+        if(trans.position.x < -10)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
