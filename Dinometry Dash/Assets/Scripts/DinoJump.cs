4a033fc6f5b64785fb79aa649f9b3d94c54fe9cc
AeroSoap <joshua.adler@digipen.edu>
2020-02-05 12:32:59 -0800

    Made changes

--- Dinometry Dash/Assets/Scripts/DinoJump.cs
+++ Dinometry Dash/Assets/Scripts/DinoJump.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class DinoJump : MonoBehaviour
 {
+
+    const int STANDING = 0;
+    const int RUNNING = 1;
+    const int CROUCHING = 2;
+    const int OOF = 3;
+
+    public Sprite Standing;
+    public Sprite Running0;
+    public Sprite Running1;
+    public Sprite Crouching0;
+    public Sprite Crouching1;
+    public Sprite Oof;
+
+
+    int animState;
+    int subState;
+
     public float JumpPower;
     public float DownPower;
     public int JumpFrames;
@@ -18,10 +35,13 @@ public class DinoJump : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Time.timeScale = 0;
         groundCol = Ground.GetComponent<Collider2D>();
         dinoCol = GetComponent<Collider2D>();
         rb = GetComponent<Rigidbody2D>();
         jfs = 0;
+        animState = 0;
+        subState = 0;
     }
 
     // Update is called once per frame
