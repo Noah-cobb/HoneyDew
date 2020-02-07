4f5b5669ef00b9c401b42bb1b1bb2196f9ef0d68
Tom16980 <Thomas.farrington@digipen.edu>
2020-02-07 13:26:29 -0800

    Added sounds

---
+++ Dinometry Dash/Assets/Scripts/RandomContainer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//grants acess to mixer
+using UnityEngine.Audio;
+
+public class RandomContainer : MonoBehaviour
+{
+
+    public AudioClip[] clips;
+    public AudioMixerGroup output;
+    public KeyCode keyToPress = KeyCode.Space;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //default playback method for testing
+        if (Input.GetKeyDown(keyToPress))
+        {
+            PlaySound();
+        }
+
+    }
+    public void PlaySound()
+    {
+        //randomize within the array length
+        int randomClip = Random.Range(0, clips.Length);
+
+        //create audiosource
+        AudioSource source = gameObject.AddComponent<AudioSource>();
+
+        //load clip into audiosource
+        source.clip = clips[randomClip];
+
+        //set output for audiosource 
+        source.outputAudioMixerGroup = output;
+
+        //play clip
+        source.Play();
+
+        //destroy audio when done, after the full length of clip
+        Destroy(source, clips[randomClip].length);
+    }
+}       
