eb537a96d17d88856806c9ca602629dda221d3e5
AeroSoap <joshua.adler@digipen.edu>
2020-02-10 13:42:35 -0800

    AAAAAAAAAAAAHHHHHHHHHHHHHH

---
+++ Galaxy Dash/Assets/Scripts/CloudMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMove : MonoBehaviour
+{
+
+    public float Speed;
+    float para;
+
+
+    Transform trans;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        para = Random.Range(3f, 10f);
+        trans = GetComponent<Transform>();
+        float scale = 5f / para;
+        gameObject.transform.localScale = new Vector3(scale, scale, scale);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        trans.position += new Vector3(Speed / para * Time.deltaTime * ObstacleMovement.Speed, 0);
+        if (trans.position.x < -22)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Galaxy Dash/Assets/Scripts/DinoJump.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class DinoJump : MonoBehaviour
+{
+
+    const int STANDING = 0;
+    const int RUNNING = 1;
+    const int CROUCHING = 2;
+    const int OOF = 3;
+    public AudioClip DinoJumpp;
+    AudioSource source;
+
+    public Sprite Standing;
+    public Sprite[] Running;
+    public Sprite[] Crouching;
+    public Sprite Oof;
+
+    public GameObject sword;
+
+    int animState;
+    float subState;
+
+    public float JumpPower;
+    public float DownPower;
+    public int JumpFrames;
+
+   
+
+
+    int jfs;
+
+    bool changedAnim;
+
+    public GameObject Ground;
+    Collider2D groundCol;
+    Collider2D dinoCol;
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        groundCol = Ground.GetComponent<Collider2D>();
+        dinoCol = GetComponent<Collider2D>();
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        jfs = 0;
+        setAnim(RUNNING);
+        subState = 0;
+
+        source = gameObject.AddComponent<AudioSource>();
+
+    }
+
+    void doAnim()
+    {
+        switch(animState)
+        {
+            case STANDING:
+                sr.sprite = Standing;
+                subState = 0;
+                break;
+            case RUNNING:
+                sr.sprite = Running[(int) subState];
+                subState = (subState + Time.deltaTime * 8) % Running.Length;
+                break;
+            case CROUCHING:
+                sr.sprite = Crouching[(int) subState];
+                subState = (subState + Time.deltaTime * 8) % Crouching.Length;
+                break;
+            case OOF:
+                sr.sprite = Oof;
+                subState = 0;
+                break;
+        }
+    }
+
+    void setAnim(int state)
+    {
+        if (!changedAnim && animState != state)
+        {
+            changedAnim = true;
+            animState = state;
+            doAnim();
+            Destroy(GetComponent<PolygonCollider2D>());
+            gameObject.AddComponent<PolygonCollider2D>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        changedAnim = false;
+        if (Time.timeScale > 0)
+        {
+            dinoCol = GetComponent<Collider2D>();
+            if (dinoCol.IsTouching(groundCol))
+            {
+                if (Input.GetKey("down"))
+                {
+                    setAnim(CROUCHING);
+                }
+                else
+                {
+                    jfs = JumpFrames;
+                    setAnim(RUNNING);
+                }
+            }
+            //jump
+            if (jfs > 0 && Input.GetKey("space"))
+            {
+                jfs--;
+                rb.velocity = new Vector2(0, JumpPower);
+                if(animState != STANDING)
+                {
+                   
+                    sword.GetComponent<Sword>().slash();
+                    source.clip = DinoJumpp;
+                    source.Play();
+                }
+                setAnim(STANDING);
+            }
+            else
+            {
+                jfs = 0;
+            }
+            if (Input.GetKey("down"))
+            {
+                rb.velocity = new Vector2(0, -DownPower);
+            }
+            doAnim();
+        }
+        else
+        {
+            setAnim(OOF);
+        }
+    }
+}
